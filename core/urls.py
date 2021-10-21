9a2ca6773e8690bd5f55c8d399fe3aa09638dbfe
Olavo-DIO <olavo.melo.br.DIO@gmail.com>
2021-10-21 13:03:20 -0300

    Projeto básico com Python & Django Framework
    desenvolvendo os templates....

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.urls import path
-from .views import index, sticky
+from .views import index, contato, detalhes, lista, login, signin
 
 
 urlpatterns = [
     path('', index, name='index'),
-    #path('', sticky, name='sticky'),
-
+    path('/contato', contato, name='contato'),
+    path('/detalhes', detalhes, name='detalhes'),
+    path('/lista', lista, name='lista'),
+    path('/login', login, name='login'),
+    path('/signin', signin, name='signin'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,6 +6,24 @@ def index(request):
     return render(request, 'index.html')
 
 
+def lista(request):
+    return render(request, 'lista.html')
 
-def sticky(request):
-    return render(request, 'sticky.html')
+
+def detalhes(request):
+    return render(request, 'detalhes.html')
+
+
+def contato(request):
+    return render(request, 'contato.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signin(request):
+    return render(request, 'signin.html')
+
+
+
